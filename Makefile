c64ed3824fe779d16f5357f19555d0777e1fdb48
xiaobai <319331998@qq.com>
2024-04-24 13:14:20 +0800

    build REPL (only ".exit" command)

---
+++ Makefile
@@ -0,0 +1,19 @@
+CC = gcc
+CFLAGS = -Wall -Wextra -Iinclude -std=c17
+SRC_DIR = src
+INC_DIR = include
+BIN_DIR = bin
+
+SRCS = $(wildcard $(SRC_DIR)/*.c)
+OBJS = $(SRCS:$(SRC_DIR)/%.c=$(BIN_DIR)/%.o)
+
+all: my_program
+
+my_program: $(OBJS) 
+	$(CC) $(CFLAGS) -o $@ $^
+
+$(BIN_DIR)/%.o: $(SRC_DIR)/%.c 
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+clean: 
+	rm -f $(BIN_DIR)/*.o my_program
---
+++ src/input_buffer.c
@@ -0,0 +1,16 @@
+#include "../include/input_buffer.h"
+#include <stdlib.h>
+
+InputBuffer *new_input_buffer() {
+  InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
+
+  return input_buffer;
+}
+
+void close_input_buffer(InputBuffer *input_buffer) {
+  free(input_buffer->buffer);
+  free(input_buffer);
+}
